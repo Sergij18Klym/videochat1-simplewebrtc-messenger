ffe239089a0949d87633ac3075641f2fbb518538
Sergij18Klym <derstig@ukr.net>
2020-11-01 21:53:57 +0100

    anuthing3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿const express = require('express');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Set public folder as root
 app.use(express.static('public'));
